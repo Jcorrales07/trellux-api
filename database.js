6e2aa94a8c73fcfb3d85e715082c125d8d7c442e
Joe Corrales <corralesjoe07@gmail.com>
2023-07-25 21:22:14 -0600

    .

--- database.js
+++ database.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+require('dotenv').config()
 
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI
 
 const db = mongoose.connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 
-module.exports = db;
 
+
+module.exports = db
--- index.js
+++ index.js
@@ -20,56 +20,9 @@ const db = require('./database')
 const routerApi = require('./routes')
 routerApi(app)
 
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
-// app.get('/register', (req, res) => {
-//     // create a new user
-//     const { _name, _lastname, _username, _email, _password } = req.body
-
-//     UserModel.create({
-//         name: _name,
-//         lastname: _lastname,
-//         username: _username,
-//         email: _email,
-//         password: UserSchema.methods.encryptPassword(_password),
-//     })
-//         .then((user) => {
-//             res.json(user)
-//         })
-//         .catch((err) => {
-//             res.send('Error creating user')
-//         })
-// })
-
-// app.get('/login', async (req, res) => {
-//     // login a user
-//     // const { _username, _password } = req.body
-
-//     let _username = 'jcorralesss'
-//     let _password = '123456'
-
-//     UserModel.findOne({ username: _username }).then((user) => {
-//         if (user) {
-//             const verify = UserSchema.methods.validatePassword(
-//                 _username,
-//                 _password
-//             )
-//             if (verify) {
-//                 res.json(user)
-//                 return
-//             }
-//             res.send('Wrong password')
-//         }
-//     })
-// })
-
-// app.get('/users', async (req, res) => {
-//     const users = await UserModel.find()
-//     res.json(users)
-// })
 
 // db connection
 // only if the db connection is successful, start the server
